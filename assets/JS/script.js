dd7d765dd638a75fc28f5aa50708354c3ff3f05b
thomas3790 <thomas.audouit@gmail.com>
2021-04-10 18:02:04 +0200

    Animation Accueil + correction/optimisation du code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -15,3 +15,22 @@ var navSlide = ()=>{ //Ma var stock la valeur d'une fonction anonyme
 }
 
 navSlide();
+
+const titleSpans = document.querySelectorAll('.citation span');
+const btns = document.querySelectorAll('.cta');
+const bulles = document.querySelectorAll('.bulle');
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+    // Pas obligé de mettre sur pause au départ mais c'eest une bonne pratique.
+
+    TL
+    .staggerFrom(titleSpans, 1, {top: -50, opacity: 0, ease:"power2.out"}, 0.3)
+    // le stagger c'est le temps entre chaque animation.
+    .staggerFrom(btns, 1, {opacity: 0, ease:"power1.out"}, 0.3, '-=1')
+    .staggerFrom(bulles, 0.7, {right: -70, opacity: 0, ease:"power2.out"}, 0.3, '-=0.6');
+    //On met le ; à la dernière animation. Pas de staggerFrom si pas besoin de définir "l'intermitence".
+
+    TL.play();
+})
